64d434fcd69484b99a015809bb7de7d276a23cfd
Jonathan Robson <jonathan.robson@remote-learner.net>
2010-08-26 09:56:45 -0500

    Return early! It helps performance!

--- sharedlib.php
+++ sharedlib.php
@@ -1,7 +1,16 @@
 <?php
+
 function is_elis() {
     global $CFG;
-    return file_exists($CFG->dirroot . '/curriculum/config.php') && record_exists('block', 'name', 'curr_admin');
+
+    if (!file_exists($CFG->dirroot.'/curriculum/config.php')) {
+        // return early!
+        return false;
+    }
+    if (!record_exists('block', 'name', 'curr_admin')) {
+        return false;
+    }
+    return true;
 }
 
 ?>
