0d700f0282401ad4e0b6a11f66c6cc5d8437109d
Hubert Chathi <hubert@remote-learner.net>
2011-02-28 14:22:21 -0500

    ELIS-1785 recode es_mx translation files to utf8, LF

--- lang/es_mx_utf8/block_rlip.php
+++ lang/es_mx_utf8/block_rlip.php
@@ -1,73 +1,73 @@
 <?php
 $string['class_properties'] = 'Mapa de las propiedades de clase';
-$string['couldnotopenexportfile'] = 'No se pudo abrir el archivo de exportación: $a';
+$string['couldnotopenexportfile'] = 'No se pudo abrir el archivo de exportaciÃ³n: $a';
 $string['course'] = 'Curso';
 $string['course_properties'] = 'Mapa de las propiedades del curso';
 $string['curr_properties'] = 'Mapa de las propiedades del plan de estudio';
-$string['dataimport'] = 'Punto de Integración';
-$string['delete_failed'] = 'No se puede eliminar el archivo de importación<br />';
+$string['dataimport'] = 'Punto de IntegraciÃ³n';
+$string['delete_failed'] = 'No se puede eliminar el archivo de importaciÃ³n<br />';
 $string['disabled'] = 'Disabled';
-$string['emailnotification'] = 'Notificación por correo electrónico';
+$string['emailnotification'] = 'NotificaciÃ³n por correo electrÃ³nico';
 $string['enabled'] = 'Habilitado';
-$string['enrolment'] = 'Inscripción';
-$string['enrol_properties'] = 'Mapa de las propiedades de inscripción';
+$string['enrolment'] = 'InscripciÃ³n';
+$string['enrol_properties'] = 'Mapa de las propiedades de inscripciÃ³n';
 
-$string['export_location_missing'] = 'Ubicación del archivo de exportación es que falta. Por favor, asegúrese rlip fue configurado correctamente.';
+$string['export_location_missing'] = 'UbicaciÃ³n del archivo de exportaciÃ³n es que falta. Por favor, asegÃºrese rlip fue configurado correctamente.';
 $string['export_now'] = 'Exportar ahora';
-$string['exportallhistorical'] = 'Incluya todos los datos históricos';
-$string['exportfilelocation'] = 'Exportar Ubicación del archivo';
-$string['exportfiletimestamp'] = 'Nombres Únicos de archivos de exportación';
-$string['exportperiod'] = 'Período de Exportación';
+$string['exportallhistorical'] = 'Incluya todos los datos histÃ³ricos';
+$string['exportfilelocation'] = 'Exportar UbicaciÃ³n del archivo';
+$string['exportfiletimestamp'] = 'Nombres Ãnicos de archivos de exportaciÃ³n';
+$string['exportperiod'] = 'PerÃ­odo de ExportaciÃ³n';
 
 $string['export_header_firstname'] = 'Nombre';
 $string['export_header_lastname'] = 'Apellido';
 $string['export_header_username'] = 'Nombre de usuario';
 $string['export_header_user_idnumber'] = 'User Idnumber';
-$string['export_header_course_idnumber'] = 'Número de identificación de curso';
+$string['export_header_course_idnumber'] = 'NÃºmero de identificaciÃ³n de curso';
 $string['export_header_start_date'] = 'Fecha de inicio';
-$string['export_header_end_date'] = 'Fecha de finalización';
+$string['export_header_end_date'] = 'Fecha de finalizaciÃ³n';
 $string['export_header_status'] = 'Estatus';
-$string['export_header_grade'] = 'Calificación';
+$string['export_header_grade'] = 'CalificaciÃ³n';
 $string['export_header_letter'] = 'Letra';
 
 $string['export_config_form_column_header'] = 'Encabezado de columna';
 $string['export_config_form_profile_field'] = 'Perfil de campo';
-$string['export_config_form_add'] = 'Añadir';
+$string['export_config_form_add'] = 'AÃ±adir';
 $string['export_config_form_update'] = 'Actualizar';
-$string['export_config_duplicate_header_error'] = 'Por favor, elija un nombre de columna única';
+$string['export_config_duplicate_header_error'] = 'Por favor, elija un nombre de columna Ãºnica';
 
-$string['export_config_instructions'] = 'En la actualidad, no hay campos de perfil se incluyen en la exportación.  Para añadir esta información a la exportación, agregue los campos que desee a través de la interfaz anterior.';
+$string['export_config_instructions'] = 'En la actualidad, no hay campos de perfil se incluyen en la exportaciÃ³n.  Para aÃ±adir esta informaciÃ³n a la exportaciÃ³n, agregue los campos que desee a travÃ©s de la interfaz anterior.';
 
 $string['mapping_column_header'] = 'Encabezado de columna';
 $string['mapping_profile_field'] = 'Perfil de campo';
 
-$string['filelocation'] = 'Importar ubicación del archivo';
+$string['filelocation'] = 'Importar ubicaciÃ³n del archivo';
 $string['filename'] = 'Nombre de archivo';
 $string['filetype'] = 'Tipo de Campo';
 $string['general'] = 'General';
-$string['generalimportinfo'] = 'Este se verifica para los archivos de formato especial en un lugar específico y importa los datos dentro de un sistema de gestión del plan de estudios.<br />';
+$string['generalimportinfo'] = 'Este se verifica para los archivos de formato especial en un lugar especÃ­fico y importa los datos dentro de un sistema de gestiÃ³n del plan de estudios.<br />';
 
 $string['import_all'] = 'Importar todo';
-$string['import_location_missing'] = 'Ubicación del archivo de importación no se encuentra. Por favor, asegúrese rlip fue configurado correctamente.';
+$string['import_location_missing'] = 'UbicaciÃ³n del archivo de importaciÃ³n no se encuentra. Por favor, asegÃºrese rlip fue configurado correctamente.';
 $string['import_save'] = 'Guardar y el procesar';
-$string['importperiod'] = 'Período de Importación';
+$string['importperiod'] = 'PerÃ­odo de ImportaciÃ³n';
 
-$string['ip_disabled_warning'] = 'ADVERTENCIA! Integración de la funcionalidad de punto es un servicio adicional pagado y no ha sido habilitado en este sitio. Como tal, la configuración en esta sección no se utilizará. Póngase en contacto con Remote-alumno para la asistencia sobre este tema a través del enlace que aparece a continuación.';
-$string['ip_enabled'] = 'Integración de la funcionalidad de punto';
-$string['ip_instructions'] = 'Integración Punto es un servicio adicional de pago que ofrece a distancia-alumno que permite la importación y exportación de datos de cumplimentación. Si esta funcionalidad Integración punto está deshabilitado, puede ponerse en contacto <a href=\"$a\" target=\"_blank\"> remoto Estudiantes </ a> para registrarse. Una vez activada, los lugares de importación y exportación de archivos puede ser configurado correctamente en esta pantalla de configuración.';
-$string['ip_link'] = 'Integración Punto página';
-$string['ip_log'] = 'Registro de Integración punto';
+$string['ip_disabled_warning'] = 'ADVERTENCIA! IntegraciÃ³n de la funcionalidad de punto es un servicio adicional pagado y no ha sido habilitado en este sitio. Como tal, la configuraciÃ³n en esta secciÃ³n no se utilizarÃ¡. PÃ³ngase en contacto con Remote-alumno para la asistencia sobre este tema a travÃ©s del enlace que aparece a continuaciÃ³n.';
+$string['ip_enabled'] = 'IntegraciÃ³n de la funcionalidad de punto';
+$string['ip_instructions'] = 'IntegraciÃ³n Punto es un servicio adicional de pago que ofrece a distancia-alumno que permite la importaciÃ³n y exportaciÃ³n de datos de cumplimentaciÃ³n. Si esta funcionalidad IntegraciÃ³n punto estÃ¡ deshabilitado, puede ponerse en contacto <a href=\"$a\" target=\"_blank\"> remoto Estudiantes </ a> para registrarse. Una vez activada, los lugares de importaciÃ³n y exportaciÃ³n de archivos puede ser configurado correctamente en esta pantalla de configuraciÃ³n.';
+$string['ip_link'] = 'IntegraciÃ³n Punto pÃ¡gina';
+$string['ip_log'] = 'Registro de IntegraciÃ³n punto';
 
-$string['logfilelocation'] = 'Registro de ubicación del archivo';
-$string['overrideelisip'] = 'Utilizar IP básica';
+$string['logfilelocation'] = 'Registro de ubicaciÃ³n del archivo';
+$string['overrideelisip'] = 'Utilizar IP bÃ¡sica';
 $string['save'] = 'Guardar';
-$string['title'] = 'Integración Punto';
+$string['title'] = 'IntegraciÃ³n Punto';
 $string['track_properties'] = 'Seguir mapa de propriedades';
 $string['user'] = 'User';
 $string['user_properties'] = 'Usar mapa de propriedades';
 
 $string['filepaths'] = 'Rutas de archivos';
-$string['configfilepaths'] = 'Estos caminos deben ser configurados por un miembro del personal a distancia-Aprendizaje para asegurar la correcta funcionalidad de la integración punto.';
+$string['configfilepaths'] = 'Estos caminos deben ser configurados por un miembro del personal a distancia-Aprendizaje para asegurar la correcta funcionalidad de la integraciÃ³n punto.';
 $string['notifications'] = 'Notificaciones';
 $string['confignotifications'] = '';
 $string['import'] = 'Importar';
@@ -75,44 +75,44 @@ $string['configimport'] = '';
 $string['export'] = 'Exportar';
 $string['configexport'] = '';
 $string['elis'] = 'ELIS';
-$string['configelis'] = 'Configuración de esta sección se aplican específicamente a los sitios de ejecución ELIS.';
+$string['configelis'] = 'ConfiguraciÃ³n de esta secciÃ³n se aplican especÃ­ficamente a los sitios de ejecuciÃ³n ELIS.';
 
-$string['configcreategroups'] = 'Si se activa,crear automáticamente nuevos grupos y agrupaciones en el curso especificado.';
-$string['configdateformat'] = 'Establezca el formato de la fecha prevista de importación.';
-$string['configemailnotification'] = 'Una lista separada por comas de los números de identificación de los usuarios a enviar el log de acciones a través de correo electrónico (en blanco sin ninguna notificación).';
-$string['configexportallhistorical'] = 'Al seleccionar esta opción se incluyen toda la información conclusión histórica, tanto en el manual y programa de ejecuciones de la exportación. Si no está seleccionado, sólo los datos modificados en el último día se incluirán en las ejecuciones manual, y sólo los datos modificados desde la última ejecución previsto se incluye el programa de ejecuciones.';
-$string['configexportfilelocation'] = 'Esto define la ruta de acceso absoluta al archivo de exportación que desea utilizar. Este ' .
-                                      'no es una ubicación de directorio, pero la ubicación exacta del archivo de exportación. ' .
-                                      'Si no desea crear un archivo de exportación, deje esta opción vacía.';
-$string['configexportfiletimestamp'] = 'Esto permite la exportación de integración punto a añadir una marca de tiempo única para exportar archivos.';
-$string['configexportperiod'] = 'Esta configuración controla la frecuencia con la exportación se ejecuta. Los valores pueden ser introducidos en la forma * d * m * h para representar días, horas y minutos. Por favor, establezca este valor en cinco minutos o más para que haya tiempo suficiente para el procesamiento.';
-$string['configfilelocation'] = 'Esto define la ruta absoluta al directorio que está marcada por la importación de archivos.';
-$string['configimportperiod'] = 'Esta configuración controla la frecuencia con la importación se ejecuta. Los valores pueden ser introducidos en la forma * d * m * h para representar días, horas y minutos. Por favor, establezca este valor en cinco minutos o más para que haya tiempo suficiente para el procesamiento.';
-$string['configlogfilelocation'] = 'Esto define el directorio para almacenar todo los archivos de registro de importación y exportación de .';
-$string['configoverrideelisip'] = 'No haga caso de ELIS IP y utilizar la IP básica (Moodle solamente)';
+$string['configcreategroups'] = 'Si se activa,crear automÃ¡ticamente nuevos grupos y agrupaciones en el curso especificado.';
+$string['configdateformat'] = 'Establezca el formato de la fecha prevista de importaciÃ³n.';
+$string['configemailnotification'] = 'Una lista separada por comas de los nÃºmeros de identificaciÃ³n de los usuarios a enviar el log de acciones a travÃ©s de correo electrÃ³nico (en blanco sin ninguna notificaciÃ³n).';
+$string['configexportallhistorical'] = 'Al seleccionar esta opciÃ³n se incluyen toda la informaciÃ³n conclusiÃ³n histÃ³rica, tanto en el manual y programa de ejecuciones de la exportaciÃ³n. Si no estÃ¡ seleccionado, sÃ³lo los datos modificados en el Ãºltimo dÃ­a se incluirÃ¡n en las ejecuciones manual, y sÃ³lo los datos modificados desde la Ãºltima ejecuciÃ³n previsto se incluye el programa de ejecuciones.';
+$string['configexportfilelocation'] = 'Esto define la ruta de acceso absoluta al archivo de exportaciÃ³n que desea utilizar. Este ' .
+                                      'no es una ubicaciÃ³n de directorio, pero la ubicaciÃ³n exacta del archivo de exportaciÃ³n. ' .
+                                      'Si no desea crear un archivo de exportaciÃ³n, deje esta opciÃ³n vacÃ­a.';
+$string['configexportfiletimestamp'] = 'Esto permite la exportaciÃ³n de integraciÃ³n punto a aÃ±adir una marca de tiempo Ãºnica para exportar archivos.';
+$string['configexportperiod'] = 'Esta configuraciÃ³n controla la frecuencia con la exportaciÃ³n se ejecuta. Los valores pueden ser introducidos en la forma * d * m * h para representar dÃ­as, horas y minutos. Por favor, establezca este valor en cinco minutos o mÃ¡s para que haya tiempo suficiente para el procesamiento.';
+$string['configfilelocation'] = 'Esto define la ruta absoluta al directorio que estÃ¡ marcada por la importaciÃ³n de archivos.';
+$string['configimportperiod'] = 'Esta configuraciÃ³n controla la frecuencia con la importaciÃ³n se ejecuta. Los valores pueden ser introducidos en la forma * d * m * h para representar dÃ­as, horas y minutos. Por favor, establezca este valor en cinco minutos o mÃ¡s para que haya tiempo suficiente para el procesamiento.';
+$string['configlogfilelocation'] = 'Esto define el directorio para almacenar todo los archivos de registro de importaciÃ³n y exportaciÃ³n de .';
+$string['configoverrideelisip'] = 'No haga caso de ELIS IP y utilizar la IP bÃ¡sica (Moodle solamente)';
 
-$string['ip_export_timespan'] = 'Nota: Cuando se ejecuta una exportación de prueba desde esta página, sólo las terminaciones de clase hasta hace un día se incluirá, a menos que el \"Guardar y procesar todos los \" botón se utiliza.';
+$string['ip_export_timespan'] = 'Nota: Cuando se ejecuta una exportaciÃ³n de prueba desde esta pÃ¡gina, sÃ³lo las terminaciones de clase hasta hace un dÃ­a se incluirÃ¡, a menos que el \"Guardar y procesar todos los \" botÃ³n se utiliza.';
 
-$string['ip_description'] = 'Nota: Cuando se ejecuta una exportación de prueba desde esta página, sólo las terminaciones de clase hasta hace un día se incluirá, a menos que el \"Guardar y procesar todos los \" botón se utiliza.
-Integración Punto se ponga los datos de entrada como campos de perfil y asigna funciones a partir de los datos importados, así como proporcionar una interfaz de registro para habilitar el soporte remoto-alumno y la prueba del lado del cliente y control de calidad y procesos en curso fracaso de recuperación. Las fichas de usuario, curso, de inscripción y contener un campo de entrada Nombre de archivo, que determina el nombre del archivo que se utiliza para la importación, dentro de la \"Situación de importación de archivos\" (situado en el punto de integración global de las opciones de bloque de configuración) de la guía. <br />
-Cada ficha también especifica la asignación entre cada uno de los campos de moodle y los campos en el archivo de importación. La importación se realiza en un horario regular cada 24 horas o cada vez que guardar e importar está seleccionada, y la exportación se ejecuta cada 24 horas. Por ejemplos de formatos de archivo por favor contacte a distancia -alumno.';
+$string['ip_description'] = 'Nota: Cuando se ejecuta una exportaciÃ³n de prueba desde esta pÃ¡gina, sÃ³lo las terminaciones de clase hasta hace un dÃ­a se incluirÃ¡, a menos que el \"Guardar y procesar todos los \" botÃ³n se utiliza.
+IntegraciÃ³n Punto se ponga los datos de entrada como campos de perfil y asigna funciones a partir de los datos importados, asÃ­ como proporcionar una interfaz de registro para habilitar el soporte remoto-alumno y la prueba del lado del cliente y control de calidad y procesos en curso fracaso de recuperaciÃ³n. Las fichas de usuario, curso, de inscripciÃ³n y contener un campo de entrada Nombre de archivo, que determina el nombre del archivo que se utiliza para la importaciÃ³n, dentro de la \"SituaciÃ³n de importaciÃ³n de archivos\" (situado en el punto de integraciÃ³n global de las opciones de bloque de configuraciÃ³n) de la guÃ­a. <br />
+Cada ficha tambiÃ©n especifica la asignaciÃ³n entre cada uno de los campos de moodle y los campos en el archivo de importaciÃ³n. La importaciÃ³n se realiza en un horario regular cada 24 horas o cada vez que guardar e importar estÃ¡ seleccionada, y la exportaciÃ³n se ejecuta cada 24 horas. Por ejemplos de formatos de archivo por favor contacte a distancia -alumno.';
 
-$string['blockname'] = 'Exportación de Terminación de Curso';
-$string['createdata'] = 'Crear los datos del usuario finalización';
-$string['createdemptyfile'] = 'Creado el archivo de exportación $ a sin datos';
+$string['blockname'] = 'ExportaciÃ³n de TerminaciÃ³n de Curso';
+$string['createdata'] = 'Crear los datos del usuario finalizaciÃ³n';
+$string['createdemptyfile'] = 'Creado el archivo de exportaciÃ³n $ a sin datos';
 $string['creategroups'] = 'Crear grupos y agrupaciones';
 $string['dateformat'] = 'Importar formato de fecha';
-$string['filenotdefined'] = 'Archivo de Exportación no se define';
-$string['filerecordwriteerror'] = 'No se puede escribir la línea $a';
+$string['filenotdefined'] = 'Archivo de ExportaciÃ³n no se define';
+$string['filerecordwriteerror'] = 'No se puede escribir la lÃ­nea $a';
 $string['filewriteerror'] = 'No se puede escribir la cabecera de archivo local $a';
 $string['localfileexists'] = 'Archivo local $a existe una, la eliminando de archivos local antes de proceder';
 $string['localfileremoved'] = 'Archivo local $a esta retirado';
 $string['localfilenotremoved'] = 'No se puede eliminar el archivo local viejo $a';
 $string['nouserdata'] = 'No hay datos de usuario para procesar';
-$string['nodata'] = 'No hay datos de finalización de usuario para crear';
-$string['noparams'] = 'Los parámetros no inicializado, no puede crear archivo csv locales para la carga';
-$string['recordadded'] = 'Datos del usuario: número de ID de usuario $a->userno - ha sido Curso número de identificación $a->coursecode añadido';
-$string['skiprecord'] = 'O bien el número identificador de usuario $a->usridnumber o número de Identificación del curso $a->crsidnumber estaba vacío - saltar registro por falta de espacio requerido (s) ';
+$string['nodata'] = 'No hay datos de finalizaciÃ³n de usuario para crear';
+$string['noparams'] = 'Los parÃ¡metros no inicializado, no puede crear archivo csv locales para la carga';
+$string['recordadded'] = 'Datos del usuario: nÃºmero de ID de usuario $a->userno - ha sido Curso nÃºmero de identificaciÃ³n $a->coursecode aÃ±adido';
+$string['skiprecord'] = 'O bien el nÃºmero identificador de usuario $a->usridnumber o nÃºmero de IdentificaciÃ³n del curso $a->crsidnumber estaba vacÃ­o - saltar registro por falta de espacio requerido (s) ';
 
-$string['nopermissions'] = 'Lo sentimos, usted no tiene los permisos necesarios para realizar esta acción';
+$string['nopermissions'] = 'Lo sentimos, usted no tiene los permisos necesarios para realizar esta acciÃ³n';
 ?>
